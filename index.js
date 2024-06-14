1504dbea6f0353172bd6d4de32324546908a2475
RabayaSultana <rabayashova811@gmail.com>
2024-06-14 16:51:28 +0600

    index.js done

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 9000
 
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Study Platform server....')
+} )
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
