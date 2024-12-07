271184c5340bd5acfc9a4fb7a4cf60b1e0d1b19e
RabayaSultana <rabayashova811@gmail.com>
2024-12-08 01:05:51 +0600

    signIn with google

--- index.js
+++ index.js
@@ -38,9 +38,15 @@ async function run() {
       // users related api
       app.post("/users", async (req, res) => {
         const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
+
 
     // get session data
     app.get("/session", async (req, res) => {
